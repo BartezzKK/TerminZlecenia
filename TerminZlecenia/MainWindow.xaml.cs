f4c075ab1f282bc3b259a4f6fd02528e243aea34
Bartek <bartosz.kk@outlook.com>
2021-08-11 23:23:30 +0200

    Better UI, error handling, cleaning code

--- TerminZlecenia/MainWindow.xaml.cs
+++ TerminZlecenia/MainWindow.xaml.cs
@@ -17,7 +17,6 @@ namespace TerminZlecenia
         {
             DataContext = this;
             InitializeComponent();
-            //lastOrder = lastDate.SelectedDate ?? DateTime.Now;
         }
 
         public IEnumerable<Order> ReadOrdersFromCSV(string fileName)
@@ -25,16 +24,21 @@ namespace TerminZlecenia
             try
             {
                 string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
-            return lines.Select(line =>
+                return lines.Select(line =>
 
-            {
-                string[] data = line.Split(';');
-                return new Order(data[0], Convert.ToInt32(data[1]), DateTime.Now);
-            });
+                {
+                    string[] data = line.Split(';');
+                    return new Order(data[0], Convert.ToInt32(data[1]), DateTime.Now);
+                });
             }
-            catch(ArgumentNullException e)
+            catch (ArgumentNullException e)
             {
-                Console.WriteLine(e);
+                MessageBox.Show("Nie wskazano żadnego pliku \n" + e.Message);
+                return null;
+            }
+            catch (FileNotFoundException e)
+            {
+                MessageBox.Show("Nieprawidłowy format pliku, wskaż plik CSV  \n" + e.Message);
                 return null;
             }
         }
@@ -44,7 +48,7 @@ namespace TerminZlecenia
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if(openFileDialog.ShowDialog() == true)
             {
-                FilePath = openFileDialog.FileName;
+                filePath = openFileDialog.FileName;
             }
             
         }
@@ -65,140 +69,72 @@ namespace TerminZlecenia
             calculateDates();
         }
 
-        //private IEnumerable<Order> calculateDates()
-        //{
-        //    int tempQuantity = averageQuantity;
-        //    int leftQuantityFromPreviousDay = 0;
-        //    DateTime tempDate = lastOrder ?? DateTime.Now;
-        //    try
-        //    {
-        //        orders = ReadOrdersFromCSV(FilePath).ToList();
-        //    }
-        //    catch (ArgumentNullException e)
-        //    {
-        //        Console.WriteLine("Nullowa wartość: " + e);
-        //    }
-        //    if (tempDate.DayOfWeek == DayOfWeek.Saturday) { tempDate.AddDays(2); }
-        //    if (tempDate.DayOfWeek == DayOfWeek.Sunday) { tempDate.AddDays(1); }
-        //    foreach (var x in orders)
-        //    {
-        //        if (tempQuantity - x.Quantity > 0 || tempQuantity - x.Quantity == 0)
-        //        {
-        //            if (leftQuantityFromPreviousDay < 0)
-        //            {
-        //                x.EstimatedEndTime = tempDate;
-        //                leftQuantityFromPreviousDay = 0;
-        //            }
-        //            else
-        //            {
-        //                x.EstimatedEndTime = tempDate;
-        //                tempQuantity -= x.Quantity;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            leftQuantityFromPreviousDay = tempQuantity - x.Quantity;
-        //            if (Math.Abs(leftQuantityFromPreviousDay) > averageQuantity)
-        //            {
-        //                if(tempDate.DayOfWeek == DayOfWeek.Thursday)
-        //                {
-        //                    tempDate = tempDate.AddDays(4);
-        //                    x.EstimatedEndTime = tempDate;
-        //                }
-        //                else if(tempDate.DayOfWeek == DayOfWeek.Friday)
-        //                {
-        //                    tempDate = tempDate.AddDays(3);
-        //                    x.EstimatedEndTime = tempDate;
-        //                }
-        //                else if(tempDate.DayOfWeek == DayOfWeek.Saturday)
-        //                {
-        //                    tempDate = tempDate.AddDays(2);
-        //                    x.EstimatedEndTime = tempDate;
-        //                }
-        //                else
-        //                {
-        //                    tempDate = tempDate.AddDays(2);
-        //                    x.EstimatedEndTime = tempDate;
-        //                }
-        //            }
-        //            else
-        //            {
-        //                tempDate = tempDate.AddDays(1);
-        //                x.EstimatedEndTime = tempDate;
-        //            }
-        //        }
-        //        if (leftQuantityFromPreviousDay < 0) { tempQuantity = averageQuantity + leftQuantityFromPreviousDay; }
-        //        if (leftQuantityFromPreviousDay > 0) { tempQuantity -= x.Quantity; }
-        //    }
-        //    ((MainWindow)Application.Current.MainWindow).UpdateLayout();
-        //    DataGridTable.ItemsSource = orders;
-        //    return orders;
-        //}
-
         private IEnumerable<Order> calculateDates()
         {
             int currentQuantity = averageQuantity;
             int leftQuantityFromPreviousDay = averageQuantity;
-            int tempDayOfWeek;
             int counterOfDays;
             //storing temporary date for calculation
-            DateTime tempDate = lastOrder ?? DateTime.Now;
-            orders = ReadOrdersFromCSV(FilePath).ToList();
-            
-            //if (tempDate.DayOfWeek == DayOfWeek.Saturday) { tempDate.AddDays(2); }
-            //if (tempDate.DayOfWeek == DayOfWeek.Sunday) { tempDate.AddDays(1); }
-            
-            foreach (var x in orders)
+            DateTime tempDate = LastOrder ?? DateTime.Now;
+            try
             {
-                tempDate = tempDate.AddDays(AddDaysIfWeekend(tempDate));
-                if (x.Quantity <= currentQuantity)
+            orders = ReadOrdersFromCSV(filePath).ToList();
+                MessageBox.Show("Sukces!");
+            }
+            catch(ArgumentNullException e)
+            {
+                MessageBox.Show("Wybierz plik CSV żeby kontynuować \n" + e.Message);
+            }
+
+            if (orders != null)
+            {
+                foreach (var x in orders)
                 {
-                    if(x.Quantity < currentQuantity)
+                    tempDate = tempDate.AddDays(addDaysIfWeekend(tempDate));
+                    if (x.Quantity <= currentQuantity)
                     {
-                        x.EstimatedEndTime = tempDate;
-                        currentQuantity -= x.Quantity;
-                        leftQuantityFromPreviousDay = currentQuantity;
-                    }
-                    else
-                    {
-                        x.EstimatedEndTime = tempDate;
-                        //tempDate = tempDate.AddDays(CountHowManyDaysShouldAdd(x.Quantity, averageQuantity, leftQuantityFromPreviousDay));
-                        tempDate = tempDate.AddDays(1);
-                        currentQuantity = averageQuantity;
-                    }
-                }
-                
-                //przewidywany czas równa się tempdate jeżeli ilość na zleceniu jest większa od średniej ilości
-                else
-                {
-                    if(currentQuantity == 0) { currentQuantity = averageQuantity; }
-                    counterOfDays = CountHowManyDaysShouldAdd(x.Quantity, averageQuantity, leftQuantityFromPreviousDay);
-                    for (int i = 1; i <= counterOfDays; i++)
-                    {
-                        
-                        if(AddDaysIfWeekend(tempDate) > 0)
+                        if (x.Quantity < currentQuantity)
                         {
-                            tempDate = tempDate.AddDays(AddDaysIfWeekend(tempDate));
-                            i--;
+                            x.EstimatedEndTime = tempDate;
+                            currentQuantity -= x.Quantity;
+                            leftQuantityFromPreviousDay = currentQuantity;
                         }
                         else
                         {
+                            x.EstimatedEndTime = tempDate;
                             tempDate = tempDate.AddDays(1);
-                            tempDate = tempDate.AddDays(AddDaysIfWeekend(tempDate));
+                            currentQuantity = averageQuantity;
                         }
                     }
-                    if(counterOfDays > 0)
-                    {
-                        currentQuantity = Math.Abs(currentQuantity) + (counterOfDays * averageQuantity) - x.Quantity;
-                    }
+
                     else
                     {
-                        currentQuantity = currentQuantity + averageQuantity - x.Quantity;
+                        if (currentQuantity == 0) { currentQuantity = averageQuantity; }
+                        counterOfDays = countHowManyDaysShouldAdd(x.Quantity, averageQuantity, leftQuantityFromPreviousDay);
+                        for (int i = 1; i <= counterOfDays; i++)
+                        {
+
+                            if (addDaysIfWeekend(tempDate) > 0)
+                            {
+                                tempDate = tempDate.AddDays(addDaysIfWeekend(tempDate));
+                                i--;
+                            }
+                            else
+                            {
+                                tempDate = tempDate.AddDays(1);
+                                tempDate = tempDate.AddDays(addDaysIfWeekend(tempDate));
+                            }
+                        }
+                        if (counterOfDays > 0)
+                        {
+                            currentQuantity = Math.Abs(currentQuantity) + (counterOfDays * averageQuantity) - x.Quantity;
+                        }
+                        else
+                        {
+                            currentQuantity = currentQuantity + averageQuantity - x.Quantity;
+                        }
+                        x.EstimatedEndTime = tempDate;
                     }
-                    x.EstimatedEndTime = tempDate;
-                    //tempDate.AddDays(AddDaysIfWeekend(tempDate));
-                    //tempDayOfWeek = CheckWhatIsCurrentDayOfWeek(tempDate);
-                    //tempDate.AddDays(CountHowManyDaysShouldAdd(x.Quantity, averageQuantity, leftQuantityFromPreviousDay));
                 }
             }
     ((MainWindow)Application.Current.MainWindow).UpdateLayout();
@@ -208,11 +144,11 @@ namespace TerminZlecenia
 
         private void calculateDates_Click(object sender, RoutedEventArgs e)
         {
-            lastOrder = lastDate.SelectedDate;
+            LastOrder = lastDate.SelectedDate;
             calculateDates();
         }
 
-        private int AddDaysIfWeekend(DateTime orderDate)
+        private int addDaysIfWeekend(DateTime orderDate)
         {
             if (orderDate.DayOfWeek == DayOfWeek.Saturday)
             { 
@@ -225,7 +161,7 @@ namespace TerminZlecenia
             else return 0;
         }
 
-        private int CountHowManyDaysShouldAdd(int quantity, int averageQuantity, int leftFromPrvDay)
+        private int countHowManyDaysShouldAdd(int quantity, int averageQuantity, int leftFromPrvDay)
         {
             if(quantity <= leftFromPrvDay)
             {
@@ -243,17 +179,15 @@ namespace TerminZlecenia
             }
         }
 
-        private int CheckWhatIsCurrentDayOfWeek(DateTime date)
+        private int checkWhatIsCurrentDayOfWeek(DateTime date)
         {
             return (int)date.DayOfWeek;
         }
 
-        private string FilePath;
+        private string filePath;
         private int averageQuantity;
         private IEnumerable<Order> orders;
-        public DateTime? lastOrder;
-
-        //public event PropertyChangedEventHandler PropertyChanged;
+        public DateTime? LastOrder;
 
     }
 }
